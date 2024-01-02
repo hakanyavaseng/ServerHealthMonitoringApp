267539a484ef1c3d049ea828946c86580136525f
Hakan Yavas <hakanyavaseng@gmail.com>
2024-01-02 11:41:13 +0300

    Fix register nulls

--- api/serverhealthapi/serverhealthapi/src/main/java/com/vtys/serverhealthapi/controller/AuthController.java
+++ api/serverhealthapi/serverhealthapi/src/main/java/com/vtys/serverhealthapi/controller/AuthController.java
@@ -1,5 +1,9 @@
 package com.vtys.serverhealthapi.controller;
 
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +47,10 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
 
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        Timestamp timestamp = Timestamp.valueOf(currentDateTime);
+
+
         if(userRepository.existsByUsername(registerDto.getUsername())){
             return ResponseEntity.badRequest().body("Username is already taken");
         }
@@ -53,8 +60,8 @@ public class AuthController {
         user.setUsername(registerDto.getUsername());
         user.setUserpassword(passwordEncoder.encode(registerDto.getPassword()));
         user.setUseremail(registerDto.getUseremail());;
-        user.setUserlastlogin("null");
-        user.setUserregistrationdate("null");
+        user.setUserlastlogin(timestamp.toString());
+        user.setUserregistrationdate(Date.valueOf(LocalDate.now()).toString());
         Roles roles = rolesRepository.findByRolename("USER").get();
         user.setRolesList(Collections.singletonList(roles));
 
