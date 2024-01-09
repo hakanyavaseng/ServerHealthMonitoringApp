7e3865156b64a0312131f6ef63b19a38f009238d
alperen güneş <Alperen GNE>
2024-01-09 22:37:15 +0300

    Cursor move added to validation

--- client/ServerHealth1/app/src/main/java/vtys/group/serverhealth/ValidationActivity.kt
+++ client/ServerHealth1/app/src/main/java/vtys/group/serverhealth/ValidationActivity.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,10 +20,47 @@ import vtys.group.serverhealth.service.impl.VerificationData
 import vtys.group.serverhealth.service.impl.RetrofitService
 
 class ValidationActivity : AppCompatActivity() {
+    private lateinit var validationFields: Array<TextInputEditText>
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_validation)
+
+        // Initialize your TextInputEditText fields
+        validationFields = arrayOf(
+            findViewById(R.id.val_input_index0),
+            findViewById(R.id.val_input_index1),
+            findViewById(R.id.val_input_index2),
+            findViewById(R.id.val_input_index3)
+        )
+
+        // Set TextWatcher for each field
+        for (i in 0 until validationFields.size - 1) {
+            validationFields[i].addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 1) {
+                        // Move focus to the next field
+                        validationFields[i + 1].requestFocus()
+                    }
+                }
+            })
+        }
+
+        // Set the last field's TextWatcher to handle the last digit
+        validationFields[validationFields.size - 1].addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // If the user enters a digit in the last field, you can handle it here.
+                // For example, you can trigger the validation process.
+            }
+        })
         val retrofitService = RetrofitService()
 
         val receivedUsername = intent.getStringExtra("USERNAME")
